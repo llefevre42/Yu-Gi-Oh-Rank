842ee5b8283863692a3f82afa19e74bf6d8a5dfb
llefevre42 <llefevre@student.42.fr>
2022-01-11 02:15:45 +0100

    premier push mobile

--- components/CellTab.js
+++ components/CellTab.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
+import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
+
 
 const CellTab = styled.div`
 font-family: "Metropolis";
 font-weight: bold;
 color: #efefef;
 width: ${props => props.size || 100}; 
-font-size: ${props => props.head ? "23px" : "25px" }; 
+font-size: ${props => props.head ? (isMobile ? "14px" :"23px") : (isMobile? "17px" :"25px") }; 
 text-align: center; 
 margin-top: 10px;
 margin-bottom: 10px; 
--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react';
 import styles from '../Styles/globalStyle'
 import Arbo from './Arbo';
 import Footer from './Footer';
+import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
 
 export default function Logo(props) {
     const [visible, setVisible] = useState(false);
@@ -13,8 +14,8 @@ export default function Logo(props) {
             overflow: "hidden",
             backgroundColor: "#22171c",
             backgroundImage: "url(" + "/pattern.png" + ")",
-            width: "100%",
-            height: "100%",
+            width: (isMobile ? "50%" : "100%"),
+            height: (isMobile ? "50%" : "100%"),
             backgroundRepeat: "no-repeat",
             backgroundSize: "30%",
             backgroundPosition: "right top",
@@ -24,15 +25,31 @@ export default function Logo(props) {
                 flexDirection: "row",
             }}>
                 <div>
-                    <img src={'/menu.png'} style={{ position: 'absolute', width: 50, height: 50, marginTop: 10, marginLeft: 10, ...styles.bordure_g, borderRadius: "20px" }} onClick={() => setVisible(!visible)} />
+                    <img src={'/menu.png'}
+                        style={{
+                            position: 'absolute',
+                            width: 50,
+                            height: 50,
+                            marginTop: 10,
+                            marginLeft: 10,
+                            ...styles.bordure_g,
+                            borderRadius: "20px"
+                        }}
+                        onClick={() => setVisible(!visible)} />
                     {visible ?
                         <Arbo></Arbo>
                         : null}
                 </div>
-                <div style={{ width: '100%', marginLeft: 20}}>
+                <div style={{ width: '100%', marginLeft: 20 }}>
                     <Link href="/">
                         <a>
-                            <img src={'/logo.png'} style={{ marginLeft: "30vw", width: 240, height: 180, marginTop: 10, marginBottom: 20 }} />
+                            <img src={'/logo.png'} 
+                            style={{ 
+                                marginLeft: "30vw", 
+                                width: (isMobile ? 120 : 240), 
+                                height: (isMobile ? 90 : 180), 
+                                marginTop: 10, 
+                                marginBottom: 20 }} />
                         </a>
                     </Link>
                     {props.children}
