d0a60e1c97c30e6160207df4bdfaacc98cab5313
llefevre42 <llefevre@student.42.fr>
2022-01-23 18:32:33 +0100

    teste admin

--- reserveFonction/AlgoPoint.js
+++ reserveFonction/AlgoPoint.js
@@ -2,8 +2,8 @@
 import isPositiveOrBeZero from './isPositiveOrBeZero'
 
 export default function AlgoPoint(joueur, place, toped, topcut) {
-
-    let resultat = (Math.trunc(joueur / 8) - (place - 1)) * 1  
+    console.log(joueur, place, toped, topcut)
+    let resultat = isPositiveOrBeZero((Math.trunc(joueur / 8) - (place - 1))) * 1  
     if(toped > 0)
          resultat + isPositiveOrBeZero((topcut / 2) - (toped - 1)) * 1
     return (resultat)
